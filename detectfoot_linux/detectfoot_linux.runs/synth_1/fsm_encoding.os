06217d5d87caf9711b74c36bb0906b4f3f2ffb94
luciferkonn <luciferkonn@163.com>
2016-09-21 09:23:50 +0800

    first commit of detectfoot_linux

---
+++ detectfoot_linux/detectfoot_linux.runs/synth_1/fsm_encoding.os
@@ -0,0 +1,15 @@
+
+ add_fsm_encoding \
+       {axi_data_fifo_v2_1_axic_reg_srl_fifo.state} \
+       { }  \
+       {{00 1000} {01 0100} {10 0001} {11 0010} }
+
+ add_fsm_encoding \
+       {axi_data_fifo_v2_1_axic_reg_srl_fifo__parameterized0.state} \
+       { }  \
+       {{00 1000} {01 0100} {10 0001} {11 0010} }
+
+ add_fsm_encoding \
+       {axi_data_fifo_v2_1_axic_reg_srl_fifo__parameterized1.state} \
+       { }  \
+       {{00 1000} {01 0100} {10 0001} {11 0010} }
---
+++ detectfoot_linux/detectfoot_linux.sdk/detectfoot/src/helloworld.c
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2012 Xilinx, Inc.  All rights reserved.
+ *
+ * Xilinx, Inc.
+ * XILINX IS PROVIDING THIS DESIGN, CODE, OR INFORMATION "AS IS" AS A
+ * COURTESY TO YOU.  BY PROVIDING THIS DESIGN, CODE, OR INFORMATION AS
+ * ONE POSSIBLE   IMPLEMENTATION OF THIS FEATURE, APPLICATION OR
+ * STANDARD, XILINX IS MAKING NO REPRESENTATION THAT THIS IMPLEMENTATION
+ * IS FREE FROM ANY CLAIMS OF INFRINGEMENT, AND YOU ARE RESPONSIBLE
+ * FOR OBTAINING ANY RIGHTS YOU MAY REQUIRE FOR YOUR IMPLEMENTATION.
+ * XILINX EXPRESSLY DISCLAIMS ANY WARRANTY WHATSOEVER WITH RESPECT TO
+ * THE ADEQUACY OF THE IMPLEMENTATION, INCLUDING BUT NOT LIMITED TO
+ * ANY WARRANTIES OR REPRESENTATIONS THAT THIS IMPLEMENTATION IS FREE
+ * FROM CLAIMS OF INFRINGEMENT, IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ */
+
+#include <stdio.h>
+
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/moduleparam.h>
+#include <linux/platform_device.h>
+#include <linux/seq_file.h>
+#include <linux/proc_fs.h>
+#include <linux/err.h>
+#include <linux/slab.h>
+#include <linux/fs.h>
+#include <linux/cdev.h>
+#include <linux/dma-mapping.h>
+#include <linux/dmapool.h>
+#include <linux/mutex.h>
+#include <linux/sched.h>
+#include <linux/wait.h>
+#include <asm-generic/uaccess.h>
+#include <asm-generic/sizes.h>
+#include <asm-generic/dma.h>
+#include <asm-generic/io.h>
+#include <linux/amba/pl330.h>
+#include <linux/of.h>
+
+int main()
+{
+    printf("Hello World\n");
+
+    return 0;
+}
